281eef947e90eb7d7e09386fdccfc70e73d02895
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-01-26 14:49:11 +0300

    2nd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// /etc/tor/bridges.conf -> /home/hardcase/.config/arti
+// cargo run --  --from-path /etc/tor/bridges.conf --to-path  /home/hardcase/.config/arti
+use clap::Parser;
+use std::path;
+
+///Copy bridges from Tor file into Arti config file
+#[derive(Parser)]
+struct Cli {
+    /// Path to Tor bridges file
+    #[arg(short, long)]
+    from_path: path::PathBuf,
+    /// Path to Arti config file
+    #[arg(short, long)]
+    to_path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("from: {:?}, into: {:?}", cli.from_path, cli.to_path);
 }
